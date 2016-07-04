69f597621e491dde3f20c7248d821a8a4663ab58
Duy Tran <duy@strayharbor.com>
2016-07-04 12:07:49 -0700

    sure let's debug in production

--- server/blueprints/root.py
+++ server/blueprints/root.py
@@ -76,6 +76,8 @@ def claim_item():
     mail = Mail(from_email, subject, to_email, content)
     res = sg.client.mail.send.post(request_body=mail.get())
 
+    return '%d %s' % (res.status_code, res.body)
+
     if res.status_code != 202:
         abort(500)
 
