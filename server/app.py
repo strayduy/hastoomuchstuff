c2e3bfa81bdf10a52d258e320a26010fb6536376
duy <duy@strayharbor.com>
2016-07-12 21:15:07 -0700

    Adding gzip compression and cache busting

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 # Third party libs
 from flask import Flask
+from flask.ext import cache_bust
+from flask.ext.compress import Compress
 
 # Blueprints
 from .blueprints import root
@@ -8,7 +10,14 @@ def create_app(config_object):
     app = Flask(__name__, static_folder='../client/static')
     app.config.from_object(config_object)
 
+    # Blueprints
     app.register_blueprint(root.blueprint)
+
+    # Cache busting
+    cache_bust.init_cache_busting(app)
+
+    # Gzip compression
+    Compress(app)
 
     return app
 
